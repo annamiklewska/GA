90f1dff0679a601d4fbd17a4d2c5e21168b20036
Ania908 <takietam908@gmail.com>
2019-03-25 16:21:47 +0100

    generating points as a class first version

--- generate_line.py
+++ generate_line.py
@@ -2,19 +2,69 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 
-class Points:
-    x_axis = np.linspace(0, 10, 100)
 
-    def polynomial(self, x, w):
-        dm = [w[i] * x**i for i in range(np.shape(w)[0])]
+class Points:
+    domain = np.linspace(0, 10, 100)  # 100 evenly distributed points in range 0-10
+
+    def __init__(self, M):
+        '''
+        :param M: degree of the polynomial separating points
+        '''
+        self.M = M
+        self.w = Points.generate_parameters(self.M)
+        self.y_above, self.y_below = Points.generate_points(self.w)
+        pass
+
+    @staticmethod
+    def polynomial(x, w):
+        '''
+        :param x: vector of arguments (starting with x^0); Nx1
+        :param w: vector of parameters; (M-1)x1
+        :return: vector of polynomial values for points x; Nx1
+        w[0]x[M-1] + w[1]x[M-1] + ... + w[N]x[0]
+        w[0]x^M + w[1]x^M-1 + ... + w[N]x^0
+        '''
+        dm = [w[i] * x ** i for i in range(len(w))]  # np.shape(w)[0] = no. of rows
         return np.sum(dm, axis=0)
 
+    def draw(self):
+        plt.scatter(Points.domain, self.y_below, c='g')
+        plt.scatter(Points.domain, self.y_above, c='r')
+        plt.plot(Points.domain, Points.polynomial(Points.domain, self.w))
+        plt.show()
 
+    @staticmethod
+    def generate_points(w):
+        # m = random.random() * 10 * random.choice((-1, 1))
+        # b = random.random() * 10
+        # w = np.array([b, m])
+
+        domain = np.linspace(0, 10, 100)  # 100 evenly distributed points in range 0-10
+        # y_above = [y(x, m, b) + (abs(random.gauss(10, 50))+5) for x in domain]
+        # y_below = [y(x, m, b) - (abs(random.gauss(10, 50))+5) for x in domain]
+        y_above = [Points.polynomial(domain, w)[i] + (abs(random.gauss(10, 50))) + 5 for i in range(len(domain))]
+        y_below = [Points.polynomial(domain, w)[i] - (abs(random.gauss(10, 50))) + 5 for i in range(len(domain))]
+        return y_above, y_below
+
+    @staticmethod
+    def generate_parameters(N):
+        '''
+        :param N: no of parameters
+        :return: vector of arguments (starting with x^0); Nx1
+        '''
+        w = np.zeros(N)
+        for i in range(N):
+            w[i] = random.random() * 10 * random.choice((-1, 1))
+        return w
+
+
+'''
 def polynomial(x, w):
     dm = [w[i] * x**i for i in range(len(w))]  # np.shape(w)[0] = no. of rows
     return np.sum(dm, axis=0)
+'''
 
-
+'''
 def solution02 ():
 
     def y(x, m, b):
@@ -24,21 +74,21 @@ def solution02 ():
     b = random.random()*10
     w = np.array([b, m])
 
-    X = np.linspace(0, 10, 100) # 100 evenly distributed points in range 0-10
-    #y_above = [y(x, m, b) + (abs(random.gauss(10, 50))+5) for x in X]
-    #y_below = [y(x, m, b) - (abs(random.gauss(10, 50))+5) for x in X]
-    y_above = [polynomial(X, w) + (abs(random.gauss(10, 50)) + 5)]
-    y_below = [polynomial(X, w) - (abs(random.gauss(10, 50)) + 5)]
+    domain = np.linspace(0, 10, 100) # 100 evenly distributed points in range 0-10
+    #y_above = [y(x, m, b) + (abs(random.gauss(10, 50))+5) for x in domain]
+    #y_below = [y(x, m, b) - (abs(random.gauss(10, 50))+5) for x in domain]
+    y_above = [polynomial(domain, w)[i] + (abs(random.gauss(10, 50))) + 5 for i in range(len(domain))]
+    y_below = [polynomial(domain, w)[i] - (abs(random.gauss(10, 50))) + 5 for i in range(len(domain))]
 
-    import matplotlib.pyplot as plt
-    plt.scatter(X, y_below, c='g')
-    plt.scatter(X, y_above, c='r')
+
+    plt.scatter(domain, y_below, c='g')
+    plt.scatter(domain, y_above, c='r')
     #plt.plot(X, y(X, m, b))
-    plt.plot(X, polynomial(X, w))
+    plt.plot(domain, polynomial(domain, w))
     plt.show()
-    print(polynomial(X, w))
+'''
 
-    '''
+'''
     def solution00():
         m, b = 1, 0
         lower, upper = -25, 25
@@ -54,4 +104,4 @@ def solution02 ():
         plt.plot(np.arange(10), m * np.arange(10) + b)
         plt.scatter(x1, y1, c='blue')
         plt.scatter(x2, y2, c='red')
-    '''
+'''
